4785ffaa8a88d33fc89cae9ae141aac19fde77cc
Olsen, Andrew H <dholsen@iu.edu>
2019-04-01 16:44:36 -0400

    Add files via upload

---
+++ Window.py
@@ -0,0 +1,16 @@
+# takes the Board and changes data into pygame
+
+import pygame
+# key-based Tetris
+from pygame import key
+# this line replaces pygame.display.init()
+# pygame.init()
+
+# TIME STUFF---------------------------------------------------------------
+# https://www.pygame.org/docs/ref/time.html#module-pygame.time
+# pygame.time.get_ticks() returns the time in milliseconds since pygame.init()
+# DISPLAY STUFF------------------------------------------------------------
+# https://www.pygame.org/docs/ref/display.html#module-pygame.display
+
+# to quit
+# pygame.display.quit
